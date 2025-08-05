e47aef87d6596155a9ecfd137e23e60596c25274
Antonio Rudas <antoniojrudass@gmail.com>
2025-08-05 16:29:15 -0500

    feat::funcion updateProject

--- src/controllers/projects.controller.js
+++ src/controllers/projects.controller.js
@@ -9,6 +9,15 @@ const getProjects = async (req, res) => {
   }
 };
 
+const getProjectById = async (req, res) => {
+  try {
+    const project = await Project.findByPk(req.params.id);
+    res.json(project);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 const createProject = async (req, res) => {
   const { name, priority, description } = req.body;
   try {
@@ -23,23 +32,29 @@ const createProject = async (req, res) => {
   }
 };
 
-const getProjectById = async (req, res) => {
+const updateProject = async (req, res) => {
+  const { name, priority, description } = req.body;
+  const { id } = req.params;
   try {
-    const project = await Project.findByPk(req.params.id);
+    const project = await Project.findByPk(id);
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+    project.name = name;
+    project.priority = priority;
+    project.description = description;
+    await project.save();
     res.json(project);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-const updateProject = async (req, res) => {
-  res.send(`Updating project with ID: ${req.params.id}`);
-};
-
 const deleteProject = async (req, res) => {
+  const { id } = req.params;
   try {
     await Project.destroy({
-      where: { id: req.params.id },
+      where: { id },
     });
     res.status(204).send();
   } catch (error) {
